5b6e48dbb17bdbf0f9851c05e1a429d3677b8ce3
onepisYa <pis1@qq.com>
2023-01-10 13:34:09 +0800

    chore(HelloWorld): debounce computed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,92 +1,66 @@
+<!--
+ * Copyright (c) 2023 by onepisYa pis1@qq.com, All Rights Reserved. 
+ * @Date: 2023-01-10 10:53:31
+ * @LastEditors: onepisYa pis1@qq.com
+ * @LastEditTime: 2023-01-10 13:32:54
+ * @FilePath: /vue3_vite_js_playground/src/components/HelloWorld.vue
+ * 路漫漫其修远兮，吾将上下而求索。
+ * @Description: 
+-->
 <script setup>
 import {
   ref,
-  reactive,
-  watchEffect,
-  onRenderTracked,
-  onRenderTriggered,
 } from 'vue';
+import { watchDebounced, useDebounceFn, computedAsync } from '@vueuse/core'
 
 defineProps({
   msg: String,
 });
-
-const state = reactive({
-  count: 0,
-});
-
 const foo = ref(2);
-
-watchEffect(
-  () => {
-    console.log(state.count+foo.value);
+// 方法一 使用 vueuse 的 两个工具函数辅助完成。
+const evaluating = ref(false)
+const double_foo = computedAsync(
+  async () => {
+    // 依赖收集仅仅在 此函数最外层收集。
+    const result = foo.value
+    const debouncedFn = useDebounceFn(() => {
+      return result * 2
+    }, 2000)
+    return debouncedFn()
   },
-  {
-    // 这两个函数的含义是  
-    // track 在于跟踪，依赖收集，看看哪些是响应式的。
-    onTrack(e) {
-      console.log(e);
-      debugger;
-      // 在 浏览器中 会在这里 停一下
-      // 然后我我们可以在 这里 看一下 e
-      // 这个 e 实际上就是 state.count
-    },
-    // trigger 在于跟踪，是什么响应式的数据发生变化，导致触发了 watchEffect 的 回调函数。
-    onTrigger(e) {
-      console.log(e);
-      // 这个 e 里面可以看到 很多东西，比如 old 和 new 的值。以及 effect 和 一些其他信息。
-      debugger;
-      // 当我们更新 属性值的时候 则会在 这里停下
-      // 这样你就 可以真的追踪你的 watcher 是如何工作的了
-      // 甚至是 组件是 如何 渲染的
-    },
-  }
-);
+  null,
+  { lazy: false, evaluating },
+)
 
-// 我们可以在 这里 调试查看 一些信息
-//  响应式调试 ？
-// 这个是 setup 中的使用方法，在选项式中，可以直接  renderTracked 和 renderTriggered 使用这两个选项就可以了。
-// 这两个 render 钩子的含义在于跟踪组件是如何被重新渲染的，是什么数据的变化导致的重渲染。
-onRenderTracked((e) => {
-  console.log(e);
-  debugger;
-});
 
-onRenderTriggered((e) => {
-  console.log(e);
-  debugger;
-});
+
+// 方式二，但是不是使用 computed
+const input = ref('')
+const updated = ref(0)
+watchDebounced(input, () => {
+  updated.value += 1
+}, { debounce: 1000, maxWait: 5000 })
 
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @click="state.count++">
-      count is {{ state.count }}
-    </button>
-      <button type="button" @click="foo++">
+    <div>evaluating: {{ evaluating }}</div>
+    <button type="button" @click="foo++">
       foo is {{ foo }}
     </button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <div>commputed_double foo {{ double_foo }}</div>
   </div>
+  <div class="card">
+    <!-- watchDebounced -->
+    <div>watchDebounced</div>
+    <input v-model="input" placeholder="Try to type anything..." type="text">
+    <div>Delay is set to 1000ms and maxWait is set to 5000ms for this demo.</div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <p>Input: {{ input }}</p>
+    <p>Times Updated: {{ updated }}</p>
+  </div>
 </template>
 
 <style scoped>
