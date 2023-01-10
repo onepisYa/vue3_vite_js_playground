d3ebfc857e52d62faaac1f0b2bb5cb6ce4d2b2bb
onepisYa <pis1@qq.com>
2023-01-10 14:28:56 +0800

    chore(HelloWorld): use refDebounce implement computedDebounce

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,16 +2,14 @@
  * Copyright (c) 2023 by onepisYa pis1@qq.com, All Rights Reserved. 
  * @Date: 2023-01-10 10:53:31
  * @LastEditors: onepisYa pis1@qq.com
- * @LastEditTime: 2023-01-10 13:37:20
+ * @LastEditTime: 2023-01-10 14:28:03
  * @FilePath: /vue3_vite_js_playground/src/components/HelloWorld.vue
  * 路漫漫其修远兮，吾将上下而求索。
  * @Description: 
 -->
 <script setup>
-import {
-  ref,
-} from 'vue';
-import { useDebounceFn, computedAsync } from '@vueuse/core'
+import { computed, ref } from 'vue';
+import { refDebounced, useDebounceFn, computedAsync } from '@vueuse/core'
 
 defineProps({
   msg: String,
@@ -32,6 +30,13 @@ const double_foo = computedAsync(
   { lazy: false, evaluating },
 )
 
+// 方法二
+const bar = ref(2)
+const debounced = refDebounced(bar, 1000)
+const double_bar = computed(() => {
+  return debounced.value * 2
+})
+
 </script>
 
 <template>
@@ -43,6 +48,15 @@ const double_foo = computedAsync(
     </button>
     <div>commputed_double foo {{ double_foo }}</div>
   </div>
+
+
+  <div class="card">
+    <button type="button" @click="bar++">
+      bar is {{ bar }}
+    </button>
+    <div>commputed_double bar{{ double_bar }}</div>
+  </div>
+
 </template>
 
 <style scoped>
