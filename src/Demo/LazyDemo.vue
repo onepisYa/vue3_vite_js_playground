3511920b58709f3521c9fa391dc95a8aaacbc795
onepisYa <pis1@qq.com>
2023-01-10 23:01:29 +0800

    chore(Demo): EagerComputer
    理解使用 EagerComputed 的场景，以及为什么 Computed 的惰性求值是什么意思，以及原理和特性。

---
+++ src/Demo/LazyDemo.vue
@@ -0,0 +1,37 @@
+<!--
+ * Copyright (c) 2023 by onepisYa pis1@qq.com, All Rights Reserved. 
+ * @Date: 2023-01-10 20:49:13
+ * @LastEditors: onepisYa pis1@qq.com
+ * @LastEditTime: 2023-01-10 21:21:05
+ * @FilePath: /vue3_vite_js_playground/src/Demo/LazyDemo.vue
+ * 路漫漫其修远兮，吾将上下而求索。
+ * @Description: 
+-->
+<script setup>
+import { computed, onUpdated } from 'vue'
+import { count } from './state'
+
+const emit = defineEmits(['update'])
+onUpdated(() => emit('update'))
+
+const isOver10 = computed(() => count.value > 5)
+/* 
+  computed 有两个特性
+  1. 同步的，缓存的，意思就是 多次读取computed 的值的时候，
+    如果 收集的 dependency 没有改变，那么直接返回 缓存的值。
+  2. 惰性求值，意思就是，computed 里面的值，仅仅会在 模板里面应用的那一瞬间
+    才会调用 getter 函数，去获取值，因为我们的 getter 函数中是 进行计算的，
+    所以计算发生在模板中调用它的时候。
+    当 dependency 改变的时候， computed 被标记为 dirty ，同理因为惰性求值，
+    所以 dependency 改变后， computed 的值是多少，我们和vue 都不知道。
+    这也就是为什么，computed 依赖改变后，返回值不变，
+    但是 该组件依然会 re-render 的原因。
+*/
+
+</script>
+
+<template>
+  <div>
+    Is over 5: {{ isOver10 }}
+  </div>
+</template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,65 +2,57 @@
  * Copyright (c) 2023 by onepisYa pis1@qq.com, All Rights Reserved. 
  * @Date: 2023-01-10 10:53:31
  * @LastEditors: onepisYa pis1@qq.com
- * @LastEditTime: 2023-01-10 14:28:03
+ * @LastEditTime: 2023-01-10 21:09:20
  * @FilePath: /vue3_vite_js_playground/src/components/HelloWorld.vue
  * 路漫漫其修远兮，吾将上下而求索。
  * @Description: 
 -->
+
+
 <script setup>
-import { computed, ref } from 'vue';
-import { refDebounced, useDebounceFn, computedAsync } from '@vueuse/core'
+
+import { ref } from 'vue'
+import { count } from '../Demo/state'
+import LazyDemo from '../Demo/LazyDemo.vue'
+import EagerDemo from '../Demo/EagerDemo.vue'
 
 defineProps({
   msg: String,
 });
-const foo = ref(2);
-// 方法一 使用 vueuse 的 两个工具函数辅助完成。
-const evaluating = ref(false)
-const double_foo = computedAsync(
-  async () => {
-    // 依赖收集仅仅在 此函数最外层收集。
-    const result = foo.value
-    const debouncedFn = useDebounceFn(() => {
-      return result * 2
-    }, 2000)
-    return debouncedFn()
-  },
-  null,
-  { lazy: false, evaluating },
-)
 
-// 方法二
-const bar = ref(2)
-const debounced = refDebounced(bar, 1000)
-const double_bar = computed(() => {
-  return debounced.value * 2
-})
 
+const lazyRenders = ref(0)
+const eagerRenders = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <div class="card">
-    <div>evaluating: {{ evaluating }}</div>
-    <button type="button" @click="foo++">
-      foo is {{ foo }}
-    </button>
-    <div>commputed_double foo {{ double_foo }}</div>
+  <div grid grid-cols-2>
+    <div>{{ msg }}</div>
+    <div class="card">
+      <span text-primary font-bold>Lazy Computed</span>
+      <div font-mono>
+        <LazyDemo @update="lazyRenders++" key="Lazy"/>
+        <div>
+          Renders: {{ lazyRenders }}
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <span text-primary font-bold>Eager Computed</span>
+      <div font-mono>
+        <EagerDemo @update="eagerRenders++" key="Eager"/>
+        <div>Renders: {{ eagerRenders }}</div>
+      </div>
+    </div>
   </div>
 
-
-  <div class="card">
-    <button type="button" @click="bar++">
-      bar is {{ bar }}
-    </button>
-    <div>commputed_double bar{{ double_bar }}</div>
+  <div class="card" mt-4 font-mono>
+    Count: {{ count }}
   </div>
-
+  <button secondary @click="count--">
+    Decrement
+  </button>
+  <button @click="count++">
+    Increment
+  </button>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
