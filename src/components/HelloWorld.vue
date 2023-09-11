3abdd0549812894029d2328f1368564ff8edaf6d
onepisYa <pis1@qq.com>
2023-09-11 16:22:42 +0800

    chore: add some drag example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,65 +2,80 @@
  * Copyright (c) 2023 by onepisYa pis1@qq.com, All Rights Reserved. 
  * @Date: 2023-01-10 10:53:31
  * @LastEditors: onepisYa pis1@qq.com
- * @LastEditTime: 2023-01-10 14:28:03
+ * @LastEditTime: 2023-09-11 16:18:46
  * @FilePath: /vue3_vite_js_playground/src/components/HelloWorld.vue
  * 路漫漫其修远兮，吾将上下而求索。
  * @Description: 
 -->
 <script setup>
-import { computed, ref } from 'vue';
-import { refDebounced, useDebounceFn, computedAsync } from '@vueuse/core'
+import { ref } from '@vue/reactivity'
+// @ts-ignore
+import { useGesture } from '@vueuse/gesture'
+import { useDrag } from '@vueuse/gesture'
+import { useMotionProperties, useSpring } from '@vueuse/motion'
+
+// @ts-ignore
+// 更多的 demo 可以查看 vueuse-gesture 的源代码
+// https://github.com/vueuse/gesture/blob/main/docs/.vitepress/components/demos
 
 defineProps({
   msg: String,
 });
-const foo = ref(2);
-// 方法一 使用 vueuse 的 两个工具函数辅助完成。
-const evaluating = ref(false)
-const double_foo = computedAsync(
-  async () => {
-    // 依赖收集仅仅在 此函数最外层收集。
-    const result = foo.value
-    const debouncedFn = useDebounceFn(() => {
-      return result * 2
-    }, 2000)
-    return debouncedFn()
-  },
-  null,
-  { lazy: false, evaluating },
-)
 
-// 方法二
-const bar = ref(2)
-const debounced = refDebounced(bar, 1000)
-const double_bar = computed(() => {
-  return debounced.value * 2
+// --------- untils ----------
+const genHandler =   ( set) => {
+  return ({ movement: [x, y], dragging })=>{
+    if (!dragging) {
+      set({ x: 0, y: 0, cursor: 'grab' })
+      return
+    }
+    set({
+      cursor: 'grabbing',
+      x,
+      y,
+    })
+  }
+
+}
+
+// ------------- state -------------
+const card = ref();
+const card_one = ref();
+
+const { motionProperties } = useMotionProperties(card, {
+  cursor: 'grab',
+  x: 0,
+  y: 0,
 })
 
+const { motionProperties: card_one_motionProperties } = useMotionProperties(card_one, {
+  cursor: 'grab',
+  x: 0,
+  y: 0,
+})
+
+// --------- logic ---------
+const { set } = useSpring(motionProperties)
+// @ts-ignore
+const { set:card_one_set } = useSpring( card_one_motionProperties )
+
+useDrag(  genHandler(set), { domTarget: card, })
+const dragHandler =  genHandler(card_one_set)
+
+// @ts-ignore
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-  <div class="card">
-    <div>evaluating: {{ evaluating }}</div>
-    <button type="button" @click="foo++">
-      foo is {{ foo }}
-    </button>
-    <div>commputed_double foo {{ double_foo }}</div>
+  <div ref="card_one" v-drag="dragHandler" class="card">
+    onepisya
   </div>
 
-
-  <div class="card">
-    <button type="button" @click="bar++">
-      bar is {{ bar }}
-    </button>
-    <div>commputed_double bar{{ double_bar }}</div>
+  <div ref="card" class="card">
+    Hello World!
   </div>
 
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
+/*
+ * Copyright (c) 2023 by onepisYa pis1@qq.com , All Rights Reserved. 
+ * @Date: 2023-09-11 14:37:35
+ * @LastEditors: onepisYa pis1@qq.com
+ * @LastEditTime: 2023-09-11 15:00:36
+ * @FilePath: /vue3_vite_js_playground/src/main.js
+ * 路漫漫其修远兮，吾将上下而求索。
+ * @Description: 
+ */
 import { createApp } from 'vue'
+import { GesturePlugin } from '@vueuse/gesture'
+import { MotionPlugin } from '@vueuse/motion'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(GesturePlugin)
+app.use(MotionPlugin)
+app.mount('#app')
